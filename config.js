8d1a6d6f621179e9321c4ee852bfdfd66f899411
kunalkw <126095927+kunalkw312@users.noreply.github.com>
2026-06-27 14:53:30 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCuB2izaoYMfr-olS3ImYL7Vw1OqkhMR5U",
+  authDomain: "qfy-leads-59c25.firebaseapp.com",
+  databaseURL: "https://qfy-leads-59c25-default-rtdb.firebaseio.com",
+  projectId: "qfy-leads-59c25",
+  storageBucket: "qfy-leads-59c25.firebasestorage.app",
+  messagingSenderId: "873319787899",
+  appId: "1:873319787899:web:3285832f2b5cda967c21de",
+  measurementId: "G-PQ108NT0YX"
+};
